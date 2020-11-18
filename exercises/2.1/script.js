68ab06b6eb8cc6379c1907139d3feb62c7ce4b82
Caue Lopes <kitoypp@gmail.com>
2020-11-18 18:59:32 -0300

    commit inicial

---
+++ exercises/2.1/script.js
@@ -0,0 +1,10 @@
+let pessoaEstudante = "euMesmo";
+let hasbilidadesPessoaEstudante = 10;
+
+//Eu sou Tryber e vou ter muito sucesso na programação !
+console.log("sucesso");
+
+var thiago = VQV;
+
+
+
